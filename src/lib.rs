351606ea588a2a841ecdaf57d810ec74804c59a1
Stefan Baumgartner <sbaumg@gmail.com>
2022-11-01 18:55:07 +0100

    lesson 2 tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 use std::{
     collections::HashMap,
-    sync::{Arc, RwLock},
+    fmt::Display,
+    sync::{Arc, PoisonError, RwLock},
 };
 
 use axum::{
     body::Bytes,
-    extract::Query,
+    extract::{Path, Query, State},
+    http::StatusCode,
     response::{Html, IntoResponse},
     routing::get,
     Router,
@@ -22,6 +24,56 @@ pub fn router(state: &SharedState) -> Router<SharedState> {
     Router::with_state(Arc::clone(&state))
         .route("/", get(hello))
         .route("/hello", get(say_hi))
+        .route("/kv/:key", get(kv_get).post(kv_set))
+}
+
+#[derive(Debug)]
+struct ErrorStatus(StatusCode);
+
+impl Display for ErrorStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<StatusCode> for ErrorStatus {
+    fn from(val: StatusCode) -> Self {
+        Self(val)
+    }
+}
+
+impl<T> From<PoisonError<T>> for ErrorStatus {
+    fn from(_: PoisonError<T>) -> Self {
+        ErrorStatus(StatusCode::INTERNAL_SERVER_ERROR)
+    }
+}
+
+impl IntoResponse for ErrorStatus {
+    fn into_response(self) -> axum::response::Response {
+        let status = self.0;
+        (status, self.to_string()).into_response()
+    }
+}
+
+async fn kv_set(
+    Path(key): Path<String>,
+    State(state): State<SharedState>,
+    bytes: Bytes,
+) -> Result<(), ErrorStatus> {
+    state.write()?.db.insert(key, bytes);
+    Ok(())
+}
+
+async fn kv_get(
+    Path(key): Path<String>,
+    State(state): State<SharedState>,
+) -> Result<Bytes, ErrorStatus> {
+    let db = &state.read()?.db;
+    if let Some(val) = db.get(&key) {
+        Ok(val.to_owned())
+    } else {
+        Err(StatusCode::NOT_FOUND.into())
+    }
 }
 
 async fn say_hi(Query(params): Query<HashMap<String, String>>) -> impl IntoResponse {
---
+++ tests/lesson-2.rs
@@ -0,0 +1,42 @@
+use axum::{
+    body::Body,
+    http::{Request, StatusCode},
+};
+
+use microservice_rust_workshop::{router, SharedState};
+use tower::Service; // for `call`
+
+#[tokio::test]
+async fn basic_db_test() {
+    let state = SharedState::default();
+    let mut app = router(&state);
+
+    // `Router` implements `tower::Service<Request<Body>>` so we can
+    // call it like any tower service, no need to run an HTTP server.
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/test")
+                .method("POST")
+                .body("Hello World".into())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/test")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    assert_eq!(&body[..], b"Hello World");
+}
