0262ab8b48a1750fa95561e59c04e90068d145d5
Stefan Baumgartner <sbaumg@gmail.com>
2022-11-01 19:07:20 +0100

    add big query

--- tests/lesson-2.rs
+++ tests/lesson-2.rs
@@ -40,3 +40,41 @@ async fn basic_db_test() {
     let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
     assert_eq!(&body[..], b"Hello World");
 }
+
+#[ignore]
+#[tokio::test]
+async fn big_request() {
+    let state = SharedState::default();
+    let mut app = router(&state);
+    let bytes = include_bytes!("../crab.png");
+
+    // `Router` implements `tower::Service<Request<Body>>` so we can
+    // call it like any tower service, no need to run an HTTP server.
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/crab")
+                .method("POST")
+                .body(bytes[..].into())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let response = app
+        .call(
+            Request::builder()
+                .uri("/kv/crab")
+                .method("GET")
+                .body(Body::empty())
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+
+    let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+    assert_eq!(body.len(), bytes.len());
+    assert_eq!(&body[..], &bytes[..]);
+}
